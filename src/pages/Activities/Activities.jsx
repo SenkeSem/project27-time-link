36f9687950840d3f6e1516addaf5688aae6dc47a
SenkeSem <sen9morf@gmail.com>
2023-11-29 19:13:45 +0300

    Добавляет дизайн секции Filter на странице Activities

--- src/pages/Activities/Activities.jsx
+++ src/pages/Activities/Activities.jsx
@@ -15,21 +15,21 @@ function Activities() {
       </div>
       <h2>Filter</h2>
       <div className={styles.filter_box}>
-        <article>
-          <div className={styles.purple_back}>Outdoor</div>
-          <div>Indoor</div>
-        </article>
-        <article>
-          <div>Passive leisure</div>
-          <div>Active leisure</div>
-        </article>
-        <article>
-          <div>Number of participants</div>
-          <div>2</div>
-        </article>
-        <article>
-          <p>14:00-17:00</p>
-        </article>
+        <ul>
+          <li>Outdoor</li>
+          <li className={styles.active}>Indoor</li>
+        </ul>
+        <ul>
+          <li className={styles.active}>Passive leisure</li>
+          <li>Active leisure</li>
+        </ul>
+        <ul>
+          <li>Number of participants</li>
+          <input type="text" placeholder="3" />
+        </ul>
+        <ul>
+          <input className={styles.timeInput} type="text" placeholder="Time" />
+        </ul>
         <div className={styles.more_box}>
           <p>More</p>
           <img src="img/right.svg" alt="right" />
